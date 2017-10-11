831646a8ded8bdefe4a316be2fe466e5432b4dc8
tonquangtu <tutq@haposoft.com>
2017-10-11 11:24:48 +0700

    edit response code

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,7 +7,7 @@ exports.userDetail = (req, res) => {
   const reqData = req.body;
   if (!reqData) {
     statusCode = 404;
-    helpers.sendResponse(res, statusCode, constants.paramValueInvalidResponse);
+    helpers.sendResponse(res, statusCode, constants.response.paramValueInvalid);
   } else {
     userService.getUserDetail(req.user.id, reqData.userId, (response) => {
       if (response.code === constants.response.userNotFound.code ||
