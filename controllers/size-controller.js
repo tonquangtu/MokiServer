f36c571520c39b802ea6bca2748bdf29068d4ede
son <vanson297.nguyen@gmail.com>
2017-10-21 14:14:57 +0700

    commit to merge

--- controllers/size-controller.js
+++ controllers/size-controller.js
@@ -3,13 +3,20 @@ const { helpers, constants } = global;
 const sizeService = require('../services/size-service');
 
 exports.getSizes = (req, res) => {
-  const categoryId = req.body.id;
+  const data = req.body;
 
-  if (!categoryId || categoryId === '') {
+  if (!data) {
+    helpers.sendResponse(res, constants.response.paramValueInvalid);
+  }
+
+  const categoryId = data.categoryId ? data.categoryId : 0;
+
+  if (categoryId === 0) {
     sizeService.getSizes((responseData) => {
       helpers.sendResponse(res, responseData);
     });
-  } if (categoryId && categoryId.length < 24) {
+  }
+  if (categoryId && categoryId.length !== 24) {
     helpers.sendResponse(res, constants.response.paramValueInvalid);
   } else {
     sizeService.getSizesByCategoryId(categoryId, (responseData) => {
