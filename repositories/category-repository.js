5dbb2e0a24bec4c76f3dc531c24fe63dc68a7d61
son <vanson297.nguyen@gmail.com>
2017-11-13 18:12:10 +0700

    add module get-categories

--- repositories/category-repository.js
+++ repositories/category-repository.js
@@ -2,14 +2,22 @@ const { Category } = global;
 
 exports.getCategoryById = categoryId => Category.findById(categoryId).exec();
 
-exports.getSizeArrayByCategoryId = (categoryId) => {
-  return Category
-    .findById(categoryId)
-    .populate({ path: 'sizes', select: 'name' })
-    .exec();
-};
+exports.getSizeArrayByCategoryId = categoryId => Category
+  .findById(categoryId)
+  .populate({ path: 'sizes', select: 'name' })
+  .exec();
 
 exports.getBrandByCategoryId = categoryId => Category
   .findById(categoryId)
   .populate({ path: 'brands' })
+  .exec();
+
+exports.getCategoryNoParentId = () => Category
+  .find({ parent: null })
+  .select('name has_brand has_name parent has_child has_size created_at sizes brands')
+  .exec();
+
+exports.getCategoryByParentId = parentIdValid => Category
+  .find({ parent: parentIdValid })
+  .select('name has_brand has_name parent has_child has_size created_at sizes brands')
   .exec();
