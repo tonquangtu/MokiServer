435ac3cd6e79f3370c5b92e6a2c812e82148868d
tonquangtu <tonquangtu49@gmail.com>
2018-08-19 13:14:59 +0700

    add promise example

--- services/brand-service.js
+++ services/brand-service.js
@@ -1,13 +1,13 @@
 const brandRepo = require('../repositories/brand-repository');
 const categoryRepo = require('../repositories/category-repository');
 
-const { constants, logger } = global;
+const {constants, logger} = global;
 
 exports.getBrandsByCategoryId = (categoryId, callback) => {
   const promise = categoryRepo.getBrandByCategoryId(categoryId);
 
   promise.then((value) => {
-    const { brands } = value;
+    const {brands} = value;
     const data = brands.map(brand => ({
       id: brand.id,
       brandName: brand.name,
@@ -47,3 +47,26 @@ exports.getBrands = (callback) => {
   });
 };
 
+// Get brand in promise style
+exports.getBrandsWithPromiseStyle = (brandId) => {
+  return new Promise((resolved, rejected) => {
+    const promise = brandRepo.getBrands();
+
+    promise.then((brands) => {
+      const data = brands.map(brand => ({
+        id: brand.id,
+        brandName: brand.name,
+      }));
+      const response = {
+        code: constants.response.ok.code,
+        message: constants.response.ok.message,
+        data,
+      };
+
+      return resolved(response);
+    }).catch((err) => {
+      logger.error('Error at function getBrands.\n', err);
+      return rejected(constants.response.systemError);
+    });
+  });
+};
